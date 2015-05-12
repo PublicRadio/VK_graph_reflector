c9376817db0648ebcda60a862e3519d435fbb308
Vsevolod Rodionov <crbrus@gmail.com>
2015-05-12 12:09:04 -0700

    updating task system

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('babel/register');
 
 require('./lib/index.js')
-    .init();
+    .init()
+    .catch(function (e) {
+        process.nextTick(function(){throw e});
+    });
 
-process.on('uncaughtException', function(e){
+process.on('uncaughtException', function (e) {
     console.error(e);
     process.exit(1);
 });
